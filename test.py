08a32f826da21bd05f46a2a683f291bfff1c048c
gk <george.kountouris@yahoo.com>
2022-11-14 11:55:28 +0200

    coomit

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import json
 import pathlib
 
+my_data_path = pathlib.Path('/home/gk/Documents/BioASQ/BioASQ-data/bioasq_factoid/Graph')
 
-from utils import utils
-# with open('/media/gkoun/BioASQ/BioASQ-data/bioasq_factoid/Graph/drkg_enhanced_logic_3_200_30_entity_embeddings.json', 'r') as f:
-#   emb = json.load(f)
+with open(my_data_path.joinpath('drkg_enhanced_logic_3_200_30_entity_embeddings.json'), 'r') as f:
+  emb = json.load(f)
 #
 # data = pickle.load(open('/media/gkoun/BioASQ/BioASQ-data/bioasq_factoid/Graph/pubmed_factoid_extracted_data_train_triplets_plus_embeddings.p', 'rb'))
 #
-# my_data_path = pathlib.Path('/media/gkoun/BioASQ/BioASQ-data/bioasq_factoid/Graph/')
+#
 #
 # with open(my_data_path.joinpath('drkg_enhanced_logic_3_200_30_entity_embeddings.json'), 'r') as f:
 #     embed = json.load(f)
@@ -34,11 +34,5 @@ from utils import utils
 #
 # print(bioask10)
 
-from pyexcel_ods3 import save_data
-from collections import OrderedDict
-data = OrderedDict() # from collections import OrderedDict
-data.update({"Sheet 1": [[1, 2, 3], [4, 5, 6]]})
-data.update({"Sheet 2": [["row 1", "row 2", "row 3"]]})
-save_data("your_file.ods", data)
 
 
